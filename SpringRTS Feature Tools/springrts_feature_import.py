be90a89f904c354d5ff8c36dc0eb2372bb82d011
Enetheru <nicholas.samuel@gmail.com>
2012-10-09 14:17:06 +1030

    more work on import, now does heirarchy and objects

--- SpringRTS Feature Tools/springrts_feature_import.py
+++ SpringRTS Feature Tools/springrts_feature_import.py
@@ -21,11 +21,15 @@ def load(context, filepath):
     # Get spring feature properties
     sfp = bpy.context.scene.sfp
 
+    #FIXME
+    meshname = ""
+
+    print("LOG: Opening luadefs file for reading")
+    print("LOG: %s" % filepath)
     f = open(filepath)
     luadef = f.read()
     f.close()
-    split = re.split('"|[ ,{}=\t\n]+|--.*',luadef)
-    
+    split = re.split('\n|[\t =",{}]+|--.*\n',luadef)
     index = 0
     while index < len(split)-1:
         key = split[index]
@@ -39,6 +43,7 @@ def load(context, filepath):
             sfp.description = pair
             index +=2
             continue
+
     # Attributes
         if key.lower() == 'damage':
             print("LOG: '%s' = %s" % (key, pair))
@@ -124,9 +129,16 @@ def load(context, filepath):
             index +=2
             continue
 
-        #FIXME ressurrectable enum
         if key.lower() == 'resurrectable':
-            print("WARN: '%s' no implemented yet" % key)
+            if pair == '-1':
+                print("LOG: '%s' = first" % key)
+                sfp.resurrectable = 'first'
+            elif pair == '0':
+                print("LOG: '%s' = no" % key)
+                sfp.resurrectable = 'no'
+            else:
+                print("LOG: '%s' = yes" % key)
+                sfp.resurrectable = 'yes'
             index+=2
             continue
 
@@ -187,29 +199,86 @@ def load(context, filepath):
             index +=2
             continue
 
-    #FIXME Collision Volume
+    #Collision Volume
         if key.lower() == 'collisionvolumetype':
-            print("WARN: '%s' no implemented yet" % key)
+            print("LOG: '%s' = %s" % (key, pair))
+            if pair == 'box':
+                sfp.collisionVolumeType = 'SME_box'
+            elif pair == 'ellipse':
+                sfp.collisionVolumeType = 'SME_ellipsoid'
+            elif pair == 'cylX':
+                sfp.collisionVolumeType = 'SME_cylX'
+            elif pair == 'cylY':
+                sfp.collisionVolumeType = 'SME_cylY'
+            else:
+                sfp.collisionVolumeType = 'SME_cylZ'
             index+=2
             continue
 
         if key.lower() == 'collisionvolumescales':
-            print("WARN: '%s' no implemented yet" % key)
+            print("LOG: '%s' = {%s, %s, %s}" % (key, 
+                split[index+1],
+                split[index+2],
+                split[index+3]))
+            sfp.collisionVolumeScales[0] = float(split[index+1])
+            sfp.collisionVolumeScales[1] = float(split[index+2])
+            sfp.collisionVolumeScales[2] = float(split[index+3])
             index+=4
             continue
 
         if key.lower() == 'collisionvolumeoffsets':
-            print("WARN: '%s' no implemented yet" % key)
+            print("LOG: '%s' = {%s, %s, %s}" % (key, 
+                split[index+1],
+                split[index+2],
+                split[index+3]))
+            sfp.collisionVolumeOffsets[0] = float(split[index+1])
+            sfp.collisionVolumeOffsets[1] = float(split[index+2])
+            sfp.collisionVolumeOffsets[2] = float(split[index+3])
             index+=4
             continue
+    # Other
+        if key.lower() == 'collisionvolumetest':
+            print("INFO: '%s' deprecated" % key)
+            index+=2
+            continue
 
-
-    #FIXME Mesh
         if key.lower() == 'object':
-            print("WARN: '%s' no implemented yet" % key)
+            print("LOG: obj filename = %s" % pair)
+            meshname = pair
             index +=2
             continue
 
+        print("LOG: '%s' - not recognised" %key)
+        index +=1
+
+    #FIXME, import mesh
+    temp = os.path.dirname(filepath)
+    temp = os.path.dirname(temp)
+    meshfilepath = temp + "/objects3d/" + meshname
+    print("LOG: importing obj mesh")
+    print("LOG: %s" % meshfilepath)
+
+    bpy.ops.import_scene.obj('EXEC_DEFAULT', filepath=meshfilepath)
+    #bpy.ops.object.transform_apply(rotation=True)
+
+    # load mesh lua def
+    meshluapath = re.sub('obj$','lua',meshfilepath)
+    print("LOG: Opening mesh lua")
+    print("LOG: %s" % meshluapath)
+
+    f = open(meshluapath)
+    meshlua = f.read()
+    f.close()
+    split = re.split('\n|[\t =",{}]+|--.*\n',meshlua)
+    index = 0
+    while index < len(split)-1:
+        key = split[index]
+        pair = split[index+1]
+        if key == '':
+            index += 1
+            continue
+
+    #Mesh
         if key.lower() == 'radius':
             print("LOG: '%s' = %s" % (key, pair))
             sfp.radius = float(pair)
@@ -217,18 +286,109 @@ def load(context, filepath):
             continue
 
         if key.lower() == 'midpos':
-            print("WARN: '%s' no implemented yet" % key)
+            print("LOG: '%s' = {%s, %s, %s}" % (key, 
+                split[index+1],
+                split[index+2],
+                split[index+3]))
+            sfp.midpos[0] = float(split[index+1])
+            sfp.midpos[1] = float(split[index+2])
+            sfp.midpos[2] = float(split[index+3])
             index+=4
             continue
 
+        if key.lower() == 'tex1':
+            print("LOG: '%s' = %s" % (key, pair))
+            sfp.tex1 = pair
+            index +=2
+            continue
+
+        if key.lower() == 'tex2':
+            print("LOG: '%s' = %s" % (key, pair))
+            sfp.tex2 = pair
+            index +=2
+            continue
     # Other
-        if key.lower() == 'collisionvolumetest':
-            print("INFO: '%s' deprecated" % key)
+        if key.lower() == 'pieces':
+            print("LOG: '%s' processed later" % key)
+            index+=1
+            continue
+
+        if key.lower() == 'offset':
+            print("WARN: '%s' not implemented yet" % key)
+            index+=4
+            continue
+
+        if key.lower() == 'numpieces':
+            print("LOG: '%s' = %s" % (key, pair))
             index+=2
             continue
+        
+        if key.lower() == 'globalvertexoffsets':
+            print("WARN: '%s' not implemented yet" % key)
+            index+=2
+            continue
+
+        if key.lower() == 'localpieceoffsets':
+            print("WARN: '%s' not implemented yet" % key)
+            index+=2
+            continue
+
 
         print("LOG: '%s' - not recognised" %key)
         index +=1
 
+    # tokenize mesh lua and clean up
+    cleansplit = []
+    split = re.split('([{}])|[= "\n\t,]+|--.*\n',meshlua)
+    for i in split:
+        if i == None:
+            continue
+        if i == '':
+            continue
+        cleansplit = cleansplit + [i]
+
+    #cut off beginning
+    begin = cleansplit.index('pieces')+2
+    sfp.rootObject = cleansplit[begin]
+    load_heirarchy(context,cleansplit, index=begin)
+
     return {'FINISHED'}
 
+def load_heirarchy(context, meshlua, parent = None, index=0):
+    obj = None
+    level = 0
+    while index < len(meshlua):
+        token = meshlua[index]
+        if token == '{':
+            level += 1
+            index += 1
+            continue
+        if token == '}':
+            level -=1
+            index+=1
+            if level <= 0: return index
+        if token == 'offset':
+            print("LOG: '%s' = {%s, %s, %s}" % (token, meshlua[index+2], meshlua[index+3], meshlua[index+4]))
+            context.scene.cursor_location[0] = float(meshlua[index+2])
+            context.scene.cursor_location[1] = float(meshlua[index+4]) * -1
+            context.scene.cursor_location[2] = float(meshlua[index+3])
+            bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+            index += 6
+            continue
+        if level == 0:
+            print("LOG: loading heirarchy info for %s" % meshlua[index])
+            # select object
+            bpy.ops.object.select_all(action='DESELECT')
+            obj = context.scene.objects[meshlua[index]]
+            obj.select = True
+            context.scene.objects.active = obj
+            bpy.ops.object.transform_apply(rotation=True)
+            if parent != None:
+                context.scene.objects.active = parent
+                bpy.ops.object.parent_set()
+            index += 1
+            continue
+        if level > 0:
+            index = load_heirarchy(context, meshlua, obj, index)
+            continue
+        index +=1
